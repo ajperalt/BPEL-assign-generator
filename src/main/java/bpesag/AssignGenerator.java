b3cc997873cf8376f3138b3046abc1004c5f4edc
unknown <mmc@sc-lpt-110.sollers.local>
2013-10-10 23:53:47 +0200

    New conception - initial commit

---
+++ src/main/java/bpesag/AssignGenerator.java
@@ -0,0 +1,14 @@
+package bpesag;
+
+public class AssignGenerator {
+
+	public static void main(String[] args) {
+		// only for testing
+		
+		AssignGenerator assignGen = new AssignGenerator();
+		
+		System.out.println("Yo!");
+
+	}
+
+}
